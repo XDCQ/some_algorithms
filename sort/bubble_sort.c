4ea26817885415ccf7a6faf2b3d6463c4b9f626b
xiadongchunqiu <xiadongchunqiu@wondersgroup.com>
2017-03-19 23:38:18 +0800

    common

--- sort/bubble_sort.c
+++ sort/bubble_sort.c
@@ -5,17 +5,25 @@
 
 void Bubble_sort(int* arr,int len)
 {
+	int flag = 0;
 	for(int i=0;i<len-1;i++)
 	{
-		for(int j=0;j<len-i;j++)
+		flag = 0;
+		for(int j=0;j<len-i-1;j++)
 		{
 			if(arr[j]>arr[j+1])
 			{
 				int temp = arr[j];
 				arr[j] = arr[j+1];
-				arr[j+1] = temp;	
+				arr[j+1] = temp;
+
+				flag = j+1;
 			}
 		}
+		if(flag == 0)
+			break;
+
+		i = len - flag - 1;
 	}
 }
 
